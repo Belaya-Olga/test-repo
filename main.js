7912ceacf1debdcc01630294c4bbb0682f368c6c
Olga Belaya <belayaolga188@gmail.com>
2025-07-16 19:07:44 +0300

    add

---
+++ main.js
@@ -0,0 +1,27 @@
+// import Swiper from "swiper";
+// import { Navigation, Pagination } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+window.addEventListener("DOMContentLoaded", () => {
+  const banner = document.querySelector(".banner");
+  banner.style.opacity = 0;
+  banner.style.transform = "translateY(20px)";
+  setTimeout(() => {
+    banner.style.transition = "all 0.6s ease";
+    banner.style.opacity = 1;
+    banner.style.transform = "translateY(0)";
+  }, 300);
+});
+
+// const swiper = new Swiper('[data-swiper="gallery"]', {
+//   slidesPerView: "auto",
+//   direction: "horizontal",
+//   spaceBetween: 10,
+//   grabCursor: true,
+//   modules: [Navigation, Pagination],
+//   navigation: {
+//     nextEl: '[data-swiper-btn-gallery="next"]',
+//     prevEl: '[data-swiper-btn-gallery="prev"]',
+//   },
+// });
